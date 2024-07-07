7ac05188a4974d24db252476ab106f5427da1c78
zuZaKuAi <125731322+zuZaKuAi@users.noreply.github.com>
2024-07-07 22:17:07 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -5,19 +5,12 @@ using namespace std;
 int main() {
 	//cin.tie(NULL);
 	//ios_base::sync_with_stdio(false);
-	int n, centor = 0, on_line = 0,t=2,temp,sum = 4;
+	int n, t = 2;
 	cin >> n;
-
-	for (int i = 0;i<n;i++)
+	for (int i = 0; i < n; i++)
 	{
-		temp = t;
-		t += pow(2,i);
-		centor = pow(t / 2, 2);
-		on_line = (temp - 1) * (temp) * 2;
-		sum += (on_line + centor);
+		t += pow(2, i);
 	}
-	
-
-		cout << sum;
+	cout << (int)pow(t, 2);
 	return 0;
 }
