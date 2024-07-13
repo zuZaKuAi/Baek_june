73f252bf86dba09805c07d22909481b92c7dbad9
zuZaKuAi <125731322+zuZaKuAi@users.noreply.github.com>
2024-07-13 22:35:18 +0900

    [Silver V] Title: 짝수? 홀수?, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/11815. 짝수？ 홀수？/짝수？ 홀수？.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+	long long n, a, a1 = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		a1 = 0;
+		cin >> a;
+		
+		if (static_cast<long long>(sqrtl(a))== sqrtl(a))
+		{
+			a1=1;
+		}
+		cout << a1 << ' ';
+	}
+}
