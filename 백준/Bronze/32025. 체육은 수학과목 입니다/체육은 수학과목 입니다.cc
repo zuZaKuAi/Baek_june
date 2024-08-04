ad56f2cb1732b54cbbe00dc31e09e328c1cb6fbf
zuZaKuAi <125731322+zuZaKuAi@users.noreply.github.com>
2024-08-04 23:35:12 +0900

    [Bronze IV] Title: 체육은 수학과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32025. 체육은 수학과목 입니다/체육은 수학과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if (x > y)
+    {
+        cout << y * 50;
+    }
+    else
+    {
+        cout << x * 50;
+    }
+    return 0;
+}
